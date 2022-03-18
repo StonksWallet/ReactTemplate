7e942a54fcc54a4b32993ccddc236bdfcea006b3
Rennan Freitas <rennanfreitas11@gmail.com>
2022-03-18 11:49:07 -0300

    Add page to account and navbar

---
+++ src/Pages/Account/Account.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+import Navbar from "../../Components/Navbar"
+
+const Account = () => {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <div>
+                Minha Conta
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Account;
