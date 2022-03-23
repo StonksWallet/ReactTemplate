a4a96e86293ff1fc41e4750b5db29c6f26cce77b
Rennan Freitas <rennanfreitas11@gmail.com>
2022-03-23 20:34:06 -0300

    Add Link in my assets, change MUI Link to React-Router Link in navbar, add Link to root in navbar title

--- src/Components/AssetsList/AssetsList.jsx
+++ src/Components/AssetsList/AssetsList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import { List, ListItem, Paper } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const AssetsList = ({ title, assets }) => {
     return (
@@ -9,29 +10,31 @@ const AssetsList = ({ title, assets }) => {
                 <p className="assets-list-title">{title}</p>
                 <List>
                     {assets.map(asset => (
-                        <ListItem
-                            key={asset.symbol}
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'space-between',
-                                minHeight: 70,
-                                backgroundColor: '#E8E8E8',
-                                marginTop: 2,
-                            }}
-                        >
-                            <span>{asset.name} ({asset.symbol})</span>
-                            <div className="assets-values">
-                                <span className="assets-price">
-                                    $ {asset.price}
-                                </span>
-                                <span style={{
-                                    color: asset.var_24h > 0 ? 'green' : 'red',
-                                    textAlign: 'start'
-                                }}>
-                                    {asset.var_24h*100}%
-                                </span>
-                            </div>
-                        </ListItem>
+                            <ListItem
+                                component={Link}
+                                to={`/assets/${asset.symbol}`}
+                                key={asset.symbol}
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    minHeight: 70,
+                                    backgroundColor: '#E8E8E8',
+                                    marginTop: 2,
+                                }}
+                            >
+                                <span className="assets-name">{asset.name} ({asset.symbol})</span>
+                                <div className="assets-values">
+                                    <span className="assets-price">
+                                        $ {asset.price}
+                                    </span>
+                                    <span style={{
+                                        color: asset.var_24h > 0 ? 'green' : 'red',
+                                        textAlign: 'start'
+                                    }}>
+                                        {asset.var_24h*100}%
+                                    </span>
+                                </div>
+                            </ListItem>
                     ))}
                 </List>
             </Paper>
