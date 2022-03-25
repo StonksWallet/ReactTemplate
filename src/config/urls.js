2b67267ce2823b65f9cfc26c8130b096d623b0df
Pedro Nóbrega <pedrohmnobrega@gmail.com>
2022-03-24 22:42:32 -0300

    Update urls.js

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,1 +1,1 @@
-export const apiUrl = process.env.NODE_ENV === 'production' ? 'https://stonks-wallet-back.herokuapp.com/' : 'http://localhost:3001/'
+export const apiUrl = 'https://stonks-wallet-back.herokuapp.com/';
